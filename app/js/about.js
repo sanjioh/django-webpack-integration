21d103ad55a8449fb36a5b6951c48d1c73bc3171
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-11-14 14:26:12 +0100

    First commit.

---
+++ app/js/about.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'from', 'About'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// async function getComponent() {
+//   const element = document.createElement('div');
+//   const { default: _ } = await import(
+//     /* webpackChunkName: "lodash" */ 'lodash'
+//   );
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+// getComponent().then(component => {
+//   document.body.appendChild(component);
+// });
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const appSrc = path.join(__dirname, 'app', 'js');
+const appDist = path.join(__dirname, 'app', 'static', 'app', 'js');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    home: path.join(appSrc, 'home.js'),
+    about: path.join(appSrc, 'about.js'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep'],
+      verbose: true,
+    }),
+  ],
+  output: {
+    path: appDist,
+    filename: '[name].js',
+    chunkFilename: '[name].non-entry-chunk.js',
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
