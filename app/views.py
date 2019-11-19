fb870bd93a4451395f92221717c942ede7849217
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-11-19 17:46:01 +0100

    Delete old Django app.

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class HomeView(TemplateView):
-    template_name = 'app/home.html'
-
-
-class AboutView(TemplateView):
-    template_name = 'app/about.html'
