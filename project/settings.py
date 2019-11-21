6264ea4b09d2ef3803ba6ca5cadef2b9dd437874
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-11-21 11:04:55 +0100

    Revert "Serve static files through Whitenoise."
    This reverts commit 98ebb48c9b8b63aabd452f5ba0d8f42880398cc0.

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
