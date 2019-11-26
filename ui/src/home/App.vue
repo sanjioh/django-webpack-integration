ce96ce9c4e8a73e80be9feaf2ed7fdffd1643ef3
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-11-26 17:04:07 +0100

    Vue.js integration.

---
+++ ui/src/home/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ItemGrid></ItemGrid>
+</template>
+
+<script>
+import ItemGrid from '@/home/components/ItemGrid.vue';
+
+export default {
+  components: {
+    ItemGrid,
+  },
+};
+</script>
+
+<style lang="sass">
+@import '@/scss/style.scss';
+</style>
---
+++ ui/src/home/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from '@/home/App.vue';
+
+new Vue({
+  el: '#app',
+  render: createElement => createElement(App),
+});
