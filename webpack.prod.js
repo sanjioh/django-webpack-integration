ab29976e585a7a66585102b7240c01c1d2a9a850
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-11-27 16:29:40 +0100

    Compress CSS in prod preserving sourcemaps.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const OptimizeCssnanoPlugin = require('@intervolga/optimize-cssnano-plugin');
 
-module.exports = merge(common, {
+module.exports = merge({
+  customizeArray(common, prod, key) {
+    if (key === 'plugins') {
+      return [...common.slice(0, -1), ...prod, ...common.slice(-1)];
+    }
+    return undefined;
+  },
+  customizeObject(common, prod, key) {
+    return undefined;
+  },
+})(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [new OptimizeCssnanoPlugin({ sourceMap: true })],
 });
